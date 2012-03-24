dfd33db8394cd6384953870e9c291bddb1738ea8
saasbook <saasbook@saasbook.(none)>
2012-03-24 13:53:36 -0700

    I've forgot about a path in cucumber :D

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -15,7 +15,8 @@ module NavigationHelpers
 
     when /^the home\s?page$/
       '/movies'
-
+    when /^the RottenPotatoes home page$/
+      '/movies'
     when /^the edit page for "([^"]+)"$/
        edit_movie_path(Movie.find_by_title($1))
 #    Add more mappings here.
