e8429d98e955b4401dce1df52746acb6c723142d
saasbook <saasbook@saasbook.(none)>
2012-03-24 10:49:09 -0700

    more specs. some fail some work :D

---
+++ spec/models/movie.rb
@@ -0,0 +1,9 @@
+requier 'spec_helper'
+
+describe Movie do
+  describe "All rating for movies" do
+    it "return all valid ratings" do
+      Movie.all_ratings.should == ["G", "PG", "PG-13", "NC-17", "R"]
+    end
+  end
+end
